ce9b2be3e7f1e3565cd41e6758033980eab8bd47
shaowei xin <2922367340@qq.com>
2022-06-08 17:11:38 +0800

    complete basic setting

--- game/views.py
+++ game/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">辛绍威的第一个网站</h1>'
+    return HttpResponse(line1)
